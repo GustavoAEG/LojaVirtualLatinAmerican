c080f786f9199ae541b334ad4b60cc75c2abf980
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-08-02 16:46:00 -0300

    #002 - CommandHandler for Produtos

--- LojaVirtual.Application/Handlers/CriarProdutoHandler.cs
+++ LojaVirtual.Application/Handlers/CriarProdutoHandler.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LojaVirtual.Application.Commands;
+using LojaVirtual.Domain.Entities;
+using LojaVirtual.Domain.Interfaces;
+using MediatR;
 
-namespace LojaVirtual.Application.Handlers
+namespace LojaVirtual.Application.Handlers.Produtos
 {
-    internal class CriarProdutoHandler
+    public class CriarProdutoHandler : IRequestHandler<CriarProdutoCommand, Guid>
     {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public CriarProdutoHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task<Guid> Handle(CriarProdutoCommand request, CancellationToken cancellationToken)
+        {
+            var produto = new Produto(
+                request.Nome,
+                request.Descricao,
+                request.Preco,
+                request.Estoque,
+                request.Tamanho,
+                request.Genero,
+                request.CategoriaId
+            );
+
+            await _produtoRepository.AdicionarAsync(produto);
+
+            return produto.Id;
+        }
     }
 }
