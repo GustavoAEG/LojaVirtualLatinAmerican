c8f5044f65a04e1c4f45878f1815dcb024339b62
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-08-02 16:57:41 -0300

    #003 - Interface Repositorio Produto

--- LojaVirtual.Domain/Interfaces/IProdutoRepository.cs
+++ LojaVirtual.Domain/Interfaces/IProdutoRepository.cs
@@ -1,16 +1,9 @@
 ﻿using LojaVirtual.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace LojaVirtual.Domain.Interfaces
 {
     public interface IProdutoRepository
     {
-        Task<Guid> AdicionarAsync(Produto produto);
-        Task<Produto?> ObterPorIdAsync(Guid id);
-        Task<IEnumerable<Produto>> ObterTodosAsync();
-        Task AtualizarAsync(Produto produto);
-        Task RemoverAsync(Guid id);
+        Task AdicionarAsync(Produto produto);
     }
 }
