ad7085bd51725f43d6b3388edd8ec21fdbc88789
Gustavo <Gustavo@WIN-216HVE2K6U1>
2025-08-02 17:04:19 -0300

    #004 - Repository Produto - AdicionarAsync method

--- LojaVirtual.Infrastructure/Repositories/ProdutoRepository.cs
+++ LojaVirtual.Infrastructure/Repositories/ProdutoRepository.cs
@@ -2,9 +2,6 @@
 using LojaVirtual.Domain.Interfaces;
 using LojaVirtual.Infrastructure.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace LojaVirtual.Infrastructure.Repositories
 {
@@ -17,37 +14,10 @@ namespace LojaVirtual.Infrastructure.Repositories
             _context = context;
         }
 
-        public async Task<Guid> AdicionarAsync(Produto produto)
+        public async Task AdicionarAsync(Produto produto)
         {
-            _context.Produtos.Add(produto);
+            await _context.Produtos.AddAsync(produto);
             await _context.SaveChangesAsync();
-            return produto.Id;
-        }
-
-        public async Task AtualizarAsync(Produto produto)
-        {
-            _context.Produtos.Update(produto);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<Produto?> ObterPorIdAsync(Guid id)
-        {
-            return await _context.Produtos.FindAsync(id);
-        }
-
-        public async Task<IEnumerable<Produto>> ObterTodosAsync()
-        {
-            return await _context.Produtos.ToListAsync();
-        }
-
-        public async Task RemoverAsync(Guid id)
-        {
-            var produto = await _context.Produtos.FindAsync(id);
-            if (produto != null)
-            {
-                _context.Produtos.Remove(produto);
-                await _context.SaveChangesAsync();
-            }
         }
     }
 }
